1a9c11576741a38c655691d6468975af9f21ad63
Gromuf <luigi.beaumois@gmail.com>
2024-11-18 11:22:14 +0100

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  return list
+    .filter((x) => x.location === city)
+    .map((x) => {
+      const gradeObj = newGrades.find((grade) => grade.studenId === x.id);
+      return {
+        ...x,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
