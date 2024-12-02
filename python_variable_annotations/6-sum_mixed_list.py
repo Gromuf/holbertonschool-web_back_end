05a6d7917b9e79234ae2d9fb2764250a8cd9da7c
Gromuf <luigi.beaumois@gmail.com>
2024-12-02 14:46:42 +0100

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""6-sum_mixed_list.py"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    res: float = 0
+    for i in mxd_lst:
+        res += i
+    return res
