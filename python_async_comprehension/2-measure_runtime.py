41fa17747f1d46510abaa4a58dc0eb09355e1207
Gromuf <luigi.beaumois@gmail.com>
2024-12-09 10:30:06 +0100

    task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+2-measure_runtime.py
+"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Measure total runtime of running async_comprehension 4 times.
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.perf_counter()
+    return end_time - start_time
