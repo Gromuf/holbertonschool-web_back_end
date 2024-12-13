eb5ac174a4304c1d4f63394af020cf584d40b075
Gromuf <luigi.beaumois@gmail.com>
2024-12-13 10:17:10 +0100

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+11-schools_by_topic.py
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of schools that have a specific topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
