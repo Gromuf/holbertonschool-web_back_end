76f85ec51efea56aa4479d2d801cd4e9b3d8af96
Gromuf <luigi.beaumois@gmail.com>
2025-01-21 10:37:25 +0100

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+// 1-stdin.js
+const readline = require("readline");
+
+const interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+interface.question("", (name) => {
+  console.log(`Your name is: ${name}`);
+  interface.close();
+});
+
+interface.on(`close`, () => {
+  console.log("This important software is now closing");
+  process.exit(0);
+});
