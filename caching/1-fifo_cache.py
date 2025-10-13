386b4173b9ffc4a1ff33fbc2d84d471feb3d414c
Gromuf <luigi.beaumois@gmail.com>
2025-10-13 14:36:53 +0200

    Fix condition in FIFO cache to prevent premature item discard

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -17,7 +17,7 @@ class FIFOCache(BaseCaching):
 
         self.cache_data[key] = item
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data))
             del self.cache_data[first_key]
             print("DISCARD: {}".format(first_key))
