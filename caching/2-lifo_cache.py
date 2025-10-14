394d4b21f1866e662bee5957e4a2c6a96abc73ca
Gromuf <luigi.beaumois@gmail.com>
2025-10-14 11:06:18 +0200

    Reorder put method logic to ensure item addition occurs after cache size check

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -16,15 +16,15 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if key not in self.cache_data:
-            self.order.append(key)
-
-        self.cache_data[key] = item
-
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             last_key = self.order.pop()
             del self.cache_data[last_key]
             print("DISCARD: {}".format(last_key))
+
+        if key not in self.cache_data:
+            self.order.append(key)
+
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
