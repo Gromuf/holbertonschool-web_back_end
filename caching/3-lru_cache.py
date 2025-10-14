010e604ee0110822cd4e819eea9852a2831d36d3
Gromuf <luigi.beaumois@gmail.com>
2025-10-14 11:23:39 +0200

    Implement LRUCache class with put and get methods for item management

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""3-lru_cache.py"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU caching system"""
+    def __init__(self):
+        """ Initialize the LRU cache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.order:
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = self.order.pop(0)
+            del self.cache_data[lru_key]
+            print("DISCARD: {}".format(lru_key))
+
+        self.order.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None:
+            return None
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+        return self.cache_data.get(key)
