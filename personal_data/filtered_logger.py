d2866091060f57378c08c168c671a2cc03f0397d
Gromuf <luigi.beaumois@gmail.com>
2025-10-20 14:48:09 +0200

    Add filtered_logger and main files for PII obfuscation functionality

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""filtered_logger.py"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """Returns the log message obfuscated"""
+    pattern = f"({'|'.join(fields)})=.*?{separator}"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
