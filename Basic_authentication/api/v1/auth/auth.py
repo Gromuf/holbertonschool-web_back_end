5d779beb0c300b965e2f0807f2ae2c6a638bb189
Gromuf <luigi.beaumois@gmail.com>
2025-10-28 10:46:28 +0100

    Remove unnecessary blank lines in auth.py

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from typing import TypeVar
 from flask import request
 
+
 class Auth:
     """Authentication class
     """
@@ -12,11 +13,9 @@ class Auth:
         """ Determine if a path requires authentication"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """ Return the Authorization header from the request"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return the current user"""
