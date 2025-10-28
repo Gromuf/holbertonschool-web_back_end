e4122130b909bd02837400e6b9db2da690187d31
Gromuf <luigi.beaumois@gmail.com>
2025-10-28 11:16:33 +0100

    Implement require_auth logic and add main script for testing

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,22 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
         """ Determine if a path requires authentication"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded in excluded_paths:
+            if excluded.endswith('*'):
+                if path.startswith(excluded[:-1]):
+                    return False
+            else:
+                if not excluded.endswith('/'):
+                    excluded += '/'
+                if path == excluded:
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Return the Authorization header from the request"""
