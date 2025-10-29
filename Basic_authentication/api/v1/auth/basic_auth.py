1d956fb41d9da206f8b77ca6d2dacdeab1a71add
Gromuf <luigi.beaumois@gmail.com>
2025-10-29 10:55:21 +0100

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,20 @@ class BasicAuth (Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decodes the Base64 string
+            base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        import base64
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
