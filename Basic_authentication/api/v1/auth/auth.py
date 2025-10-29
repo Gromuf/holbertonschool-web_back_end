95d89be69aad9ac80e1ed6d46cccef925cc146a5
Gromuf <luigi.beaumois@gmail.com>
2025-10-29 10:21:30 +0100

    task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Return the Authorization header from the request"""
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return the current user"""
