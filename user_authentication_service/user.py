7091e97cd5257359d19ddb011b91120bd236f4d4
Gromuf <luigi.beaumois@gmail.com>
2025-11-18 10:14:16 +0100

    task 0

---
+++ user_authentication_service/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class for authentication service """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), nullable=False, unique=True)
+    hashed_password = Column(String(255), nullable=False)
+    session_id = Column(String(255), nullable=True)
+    reset_token = Column(String(255), nullable=True)
+
+    def __repr__(self):
+        return "<User(id='{}', email='{}')>".format(self.id, self.email)
