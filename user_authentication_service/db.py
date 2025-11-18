0d365f445ab5d5db367674a8e088d0a2bad7a45b
Gromuf <luigi.beaumois@gmail.com>
2025-11-18 10:27:06 +0100

    task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -30,8 +30,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-        """
+        """Add a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
