0c0dfcf3692dda0dce5ff40d061f491376139502
Gromuf <luigi.beaumois@gmail.com>
2025-11-19 10:36:26 +0100

    task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -27,3 +27,12 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user login with email and password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
