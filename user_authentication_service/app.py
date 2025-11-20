f5748ab0b66f2268d8667927b293b6dba288d449
Gromuf <luigi.beaumois@gmail.com>
2025-11-20 10:04:07 +0100

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -27,5 +27,20 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """ Logs in a user and creates a session """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not email or not password:
+        flask.abort(401)
+    if not auth.valid_login(email, password):
+        flask.abort(401)
+    session_id = auth.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
