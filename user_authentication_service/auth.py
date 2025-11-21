05578cb723ba512087dd293e8e0d9154351cbdc1
Gromuf <luigi.beaumois@gmail.com>
2025-11-21 10:21:16 +0100

    task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -68,3 +68,13 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a reset password token for a user identified by email"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
