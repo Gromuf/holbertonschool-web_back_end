5ea1645ef74c5f9d9e629ad1f286c260f3a93e8c
Gromuf <luigi.beaumois@gmail.com>
2025-11-21 10:25:24 +0100

    task 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -78,3 +78,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password using a reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
