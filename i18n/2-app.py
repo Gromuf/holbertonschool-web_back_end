6a11dd63740387b1f002eae1a991340bcb923ed1
Gromuf <luigi.beaumois@gmail.com>
2025-11-27 10:29:31 +0100

    task 2

---
+++ i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" App module """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel()
+
+
+def getLocale():
+    """ Determine the best match with our supported languages. """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=getLocale)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    """ Returns a welcome message """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
