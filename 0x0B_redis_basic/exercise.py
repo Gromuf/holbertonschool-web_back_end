b962d0b91fb97ebb39d9b051384a4848c359ac74
Gromuf <luigi.beaumois@gmail.com>
2025-12-02 10:20:11 +0100

    task 0

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+exercise.py
+"""
+import redis
+import uuid
+
+class Cache:
+    """cache class to store and retrieve data using Redis"""
+    def __init__(self):
+        """ Initialize the Cache class with a Redis client """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """ Store data in Redis and return a unique key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+        
