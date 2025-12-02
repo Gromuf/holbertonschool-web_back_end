9211f035f34254899df2ab62698f5216096bc34b
Gromuf <luigi.beaumois@gmail.com>
2025-12-02 10:25:31 +0100

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -19,3 +19,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn=None) -> Union[str, bytes, int, float, None]:
+        """ Retrieve data from Redis using the given key """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ Retrieve string data from Redis using the given key """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Retrieve integer data from Redis using the given key """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return int(data)
