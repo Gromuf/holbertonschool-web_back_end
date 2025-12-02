90401ccfb632694ad5b7275fdad9ecc7231d0c05
Gromuf <luigi.beaumois@gmail.com>
2025-12-02 10:22:41 +0100

    task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,8 @@ exercise.py
 """
 import redis
 import uuid
+from typing import Union
+
 
 class Cache:
     """cache class to store and retrieve data using Redis"""
@@ -12,9 +14,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in Redis and return a unique key """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-        
