9a8abb2f31220b9cff6b88d458843b35393e7092
Gromuf <luigi.beaumois@gmail.com>
2025-12-03 11:26:19 +0100

    task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- 3-glam_rock.sql
-SELECT band_name, (COALESCE(split, YEAR(2024))) - formed AS lifespan
+SELECT band_name, (COALESCE(split, 2024) - formed) AS lifespan
 FROM metal_bands
 WHERE style = '%Glam Rock%'
 ORDER BY lifespan DESC;
