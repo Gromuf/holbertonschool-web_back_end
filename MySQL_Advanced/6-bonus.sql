0d43ba018cbf69fd84bbd1e4476aff7b74db2af2
Gromuf <luigi.beaumois@gmail.com>
2025-12-04 10:44:19 +0100

    task 6

---
+++ MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+-- 6-bonus.sql
+DDELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN bonus_score INT
+)
+BEGIN
+	DECLARE proj_id INT;
+
+	SELECT id INTO proj_id
+	FROM project
+	WHERE name = project_name;
+	LIMIT 1;
+
+	IF proj_id IS NULL THEN
+		INSERT INTO project (name)
+		VALUES (project_name);
+		SET proj_id = LAST_INSERT_ID();
+	END IF;
+
+	INSERT INTO bonus (user_id, project_id, score)
+	VALUES (user_id, proj_id, bonus_score);
+END$$
+
+DELIMITER ;
