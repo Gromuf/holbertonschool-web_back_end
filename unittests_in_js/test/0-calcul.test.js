369829b2733120dbfd008a614a89cb296507bfdc
Gromuf <luigi.beaumois@gmail.com>
2025-12-09 10:21:14 +0100

    task 0

--- unittests_in_js/test/0-calcul.test.js
+++ unittests_in_js/test/0-calcul.test.js
@@ -1,6 +1,6 @@
 // 0-calcul.test.js
 const assert = require("assert");
-const calculateNumber = require("./0-calcul");
+const calculateNumber = require("../0-calcul");
 
 describe("calculateNumber", function () {
   it("should return 5 when given 1.4 and 3.6", function () {
@@ -16,6 +16,6 @@ describe("calculateNumber", function () {
     assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
   });
   it("should return 0 when given -1.5 and 1.5", function () {
-    assert.strictEqual(calculateNumber(-1.5, 1.5), 0);
+    assert.strictEqual(calculateNumber(-1.5, 1.5), 1);
   });
 });
