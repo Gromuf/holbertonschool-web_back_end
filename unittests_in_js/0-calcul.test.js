7ff8743da134b1624f4b7e7c70642cc35e102a56
Gromuf <luigi.beaumois@gmail.com>
2025-12-09 10:26:12 +0100

    task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,19 +3,31 @@ const assert = require("assert");
 const calculateNumber = require("../0-calcul");
 
 describe("calculateNumber", function () {
-  it("should return 5 when given 1.4 and 3.6", function () {
-    assert.strictEqual(calculateNumber(1.4, 3.6), 5);
+  it("1 + 3", () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it("should return 6 when given 1.5 and 3.5", function () {
-    assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+  it("1 + 3.7 = 5", () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it("should return 0 when given 0 and 0", function () {
-    assert.strictEqual(calculateNumber(0, 0), 0);
+  it("1.2 + 3.7 = 5", () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it("should return -4 when given -1.4 and -2.6", function () {
-    assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
+  it("1.5 + 3.7 = 6", () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it("should return 0 when given -1.5 and 1.5", function () {
-    assert.strictEqual(calculateNumber(-1.5, 1.5), 1);
+  const values = [
+    -5.9, -5.5, -5.4, -5.1, -5.0, -4.9, -4.5, -4.4, -4.1, -4.0, -3.9, -3.5,
+    -3.4, -3.1, -3.0, -2.9, -2.5, -2.4, -2.1, -2.0, -1.9, -1.5, -1.4, -1.1,
+    -1.0, -0.9, -0.5, -0.4, -0.1, -0.0, 0.0, 0.1, 0.4, 0.5, 0.9, 1.0, 1.1, 1.4,
+    1.5, 1.9, 2.0, 2.1, 2.4, 2.5, 2.9, 3.0, 3.1, 3.4, 3.5, 3.9, 4.0, 4.1, 4.4,
+    4.5, 4.9, 5.0, 5.1, 5.4, 5.5, 5.9,
+  ];
+  values.forEach((a) => {
+    values.forEach((b) => {
+      const expected = Math.round(a) + Math.round(b);
+      it(`${a} + ${b} = ${expected}`, () => {
+        assert.strictEqual(calculateNumber(a, b), expected);
+      });
+    });
   });
 });
