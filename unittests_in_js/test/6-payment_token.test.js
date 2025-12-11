c5ef15c8e50e96fcd6f5cffb8759f596e24643f3
Gromuf <luigi.beaumois@gmail.com>
2025-12-11 14:51:18 +0100

    task 6

---
+++ unittests_in_js/test/6-payment_token.test.js
@@ -0,0 +1,15 @@
+// 6-payment_token.test.js
+const getPaymentTokenFromAPI = require("../6-payment_token");
+const expect = require("chai").expect;
+describe("getPaymentTokenFromAPI", function () {
+  it("should return a resolved promise with the correct data when called with true", function () {
+    return getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.deep.equal({
+        data: "Successful response from the API",
+      });
+    });
+  });
+  it("should throw an error when called with false", function () {
+    expect(() => getPaymentTokenFromAPI(false)).to.throw("API request failed");
+  });
+});
