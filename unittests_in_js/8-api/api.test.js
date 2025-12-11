0f09fd74d357dd06ba95f701a9204ee51ca3898d
Gromuf <luigi.beaumois@gmail.com>
2025-12-11 15:03:15 +0100

    task 8

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,26 @@
+// api.test.js
+const request = require("request");
+const { expect } = require("chai");
+
+describe("Index page", () => {
+  const url = "http://localhost:7865";
+  it("should return status code 200", (done) => {
+    request.get(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it("should return the correct welcome message", (done) => {
+    request.get(url, (error, response, body) => {
+      expect(body).to.equal("Welcome to the payment system");
+      done();
+    });
+  });
+  it("should not return an error", (done) => {
+    request.get(url, (error, response, body) => {
+      expect(error).to.be.null;
+      done();
+    });
+  });
+});
